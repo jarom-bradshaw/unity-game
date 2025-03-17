56f0a1290a8625de498f28e92db01f5849d755da
jacobkertesz <145492544+jacobkertesz@users.noreply.github.com>
2025-03-17 16:09:07 -0600

    UI (#26)
    * Update CameraControl.cs
    
    * Updated camera controls

--- BeatemUp/Assets/Scripts/Level/CameraControl.cs
+++ BeatemUp/Assets/Scripts/Level/CameraControl.cs
@@ -36,7 +36,7 @@ public class CameraControl : MonoBehaviour
     {
         KillEnemy();
         NoEnemies();
-        //DebugClear();
+        DebugClear();
 
         Vector3 targetPosition = target.position; // Player current position
         targetPosition.y = cameraY;
@@ -105,10 +105,10 @@ public class CameraControl : MonoBehaviour
                 clearRoom = true;
             }
     }
-    /*void DebugClear() {
+    void DebugClear() {
         if (clearRoom){
             roomState = RoomTransition.Cleared;
             clearRoom = false;
         }
-    }*/
+    }
 }
