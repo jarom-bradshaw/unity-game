3acf22c8ec7ea7cd9ba0827a7e4170afbef4d587
jacobkertesz <145492544+jacobkertesz@users.noreply.github.com>
2025-04-05 11:42:33 -0600

    Finished implementation

--- BeatemUp/Assets/Scripts/Menu/Menu.cs
+++ BeatemUp/Assets/Scripts/Menu/Menu.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     NoSQLDatabase database;
+    [SerializeField] TMP_Text timeText;
 
-    void Start()
+    async void Start()
     {
         database = new NoSQLDatabase("GameDatabase", "HighScores");
 
+        await database.LoadDatabase(); // Ensure database is loaded before fetching high score
+
         float? highScore = database.GetHighScore();
-        Debug.Log($"Current High Score: {highScore}");
+        timeText.text = $"Record: {highScore} seconds";
     }
     // Called when we click the "Play" button.
     public void OnPlayButton()
