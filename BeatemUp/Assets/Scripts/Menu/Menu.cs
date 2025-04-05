04abe3731769c2464e3d62b34113da8c27ea760f
jacobkertesz <145492544+jacobkertesz@users.noreply.github.com>
2025-04-05 11:42:56 -0600

    Color (#55)
    * changed the color of enemies
    
    * in progress implementation of database
    
    * Finished implementation

--- BeatemUp/Assets/Scripts/Menu/Menu.cs
+++ BeatemUp/Assets/Scripts/Menu/Menu.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
+    NoSQLDatabase database;
+    [SerializeField] TMP_Text timeText;
+
+    async void Start()
+    {
+        database = new NoSQLDatabase("GameDatabase", "HighScores");
+
+        await database.LoadDatabase(); // Ensure database is loaded before fetching high score
+
+        float? highScore = database.GetHighScore();
+        timeText.text = $"Record: {highScore} seconds";
+    }
     // Called when we click the "Play" button.
     public void OnPlayButton()
     {
@@ -14,4 +27,5 @@ public class Menu : MonoBehaviour
     {
         Application.Quit();
     }
+
 }
